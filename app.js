44eb131b456ef4e2c9776afdc9fee970831233b4
CamilingJS <jonathancamiling@gmail.com>
2024-11-05 12:48:53 -0800

    completed join us app

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-var express = require('express');
-var mysql = require('mysql');
-var bodyParser  = require("body-parser");
-var app = express();
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser  = require("body-parser");
+const app = express();
+require('dotenv').config();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'learnwithcolt',
-  database : 'join_us'
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 app.get("/", function(req, res){
     // Find count of users in DB
-    var q = "SELECT COUNT(*) AS count FROM users";
+    let q = "SELECT COUNT(*) AS count FROM users";
     connection.query(q, function(err, results){
         if(err) throw err;
-        var count = results[0].count; 
+        let count = results[0].count; 
         res.render("home", {count: count});
     });
 });
 
 app.post("/register", function(req, res){
-    var person = {
+    let person = {
         email: req.body.email
     };
     connection.query('INSERT INTO users SET ?', person, function(err, result) {
